f3bca203d37232e6c92f6dd956174a636aa61745
Devansh501 <81950589+Devansh501@users.noreply.github.com>
2023-09-12 22:27:02 +0530

    Update LikedCardList.jsx

--- client/src/components/LikedCardList.jsx
+++ client/src/components/LikedCardList.jsx
@@ -2,7 +2,6 @@ import LikedCard from "./LikedCard"
 
 
 const LikedCardList = ({ data, setReloader }) => {
-  console.log("hello")
   return (
     <div className="p-5 w-full sm:w-4/6 md:5/6 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4">
       {data.map((post, index) => (
